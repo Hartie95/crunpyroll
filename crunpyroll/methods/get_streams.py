d34fb02d96da2890678805a0b730bcce948d09cc
hartie95 <mail@hartie95.de>
2025-07-06 07:25:28 +0200

    [Fix] now we don't only get hardsubs anymore

--- crunpyroll/methods/get_streams.py
+++ crunpyroll/methods/get_streams.py
@@ -27,7 +27,7 @@ class GetStreams:
         await self.session.retrieve()
         response = await self.api_request(
             method="GET",
-            endpoint="v1/" + media_id + "/tv/vidaa/play",
+            endpoint="playback/v3/" + media_id + "/web/firefox/play",
             params={
                 "locale": locale or self.locale,
                 "queue": False
