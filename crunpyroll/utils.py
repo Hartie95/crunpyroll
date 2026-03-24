22dfa5f99fe8e1201976f87cbfe644ff1d3768c3
hartie95 <mail@hartie95.de>
2026-03-24 20:34:27 +0100

    [Fix] Update useragent and token (thx @stratumadev in anidl)

--- crunpyroll/utils.py
+++ crunpyroll/utils.py
@@ -2,8 +2,8 @@ from typing import Optional, List, Dict
 from datetime import datetime
 from uuid import uuid4
 
-PUBLIC_TOKEN = "Ym1icmt4eXgzZDd1NmpzZnlsYTQ6QUlONEQ1VkVfY3Awd1Z6Zk5vUDBZcUhVcllGcDloU2c="
-USER_AGENT = 'Crunchyroll/ANDROIDTV/3.42.1_22267 (Android 16; en-US; sdk_gphone64_x86_64)'
+PUBLIC_TOKEN = "bzd1b3d5N3E0bGdsdGJhdnloanE6bHFyakVUTng2Vzd1Um5wY0RtOHdSVmo4QkNoakMxZXI="
+USER_AGENT = 'Crunchyroll/ANDROIDTV/3.58.0_22336 (Android 16; en-US; sdk_gphone64_x86_64)'
 
 APP_VERSION = "3.59.0"
 
